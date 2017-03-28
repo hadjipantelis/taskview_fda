0eee6312cc2b9c43a4292e0be1a6db9bd84d08eb
fabian-s <fabian.scheipl@googlemail.com>
2017-03-28 16:52:16 +0200

    fixes #3, fixes #4

--- helper.R
+++ helper.R
@@ -4,14 +4,14 @@
 ##  vector with package names that are listed in the task view.
 extract_pkgs = function(file) {
   ll = readLines(file)
-  pkg_strings = ll[grepl("\\[\\S*\\]\\(https://.*\\)", ll)]
-  
+  pkg_strings = ll[grepl("\\[\\S*\\]\\(https://cran.*\\)", ll)]
+
   ## extract package names
   pkgs = gsub(".*\\[(\\S*)\\].*", "\\1", pkg_strings)
-  
-  # ## extract links to CRAN
-  # links = gsub(".*\\]\\((\\S*)\\).*", "\\1", pkg_strings)
-  
+
+  ## extract links to CRAN
+  #links = gsub(".*\\]\\((\\S*)\\).*", "\\1", pkg_strings)
+
   ## return package names
   pkgs
 }
@@ -22,5 +22,33 @@ get_maintainer_mail = function(pkg_name) {
   desc = packageDescription(pkg_name)
   sub(".*<(.*)>.*", "\\1", desc$Maintainer)
 }
-## test
-stopifnot(identical(get_maintainer_mail("mgcv"), "simon.wood@r-project.org"))
+
+# turn md into ctv-compliant XML
+md2ctv <- function(file, save = TRUE) {
+  pkgs = extract_pkgs(file)
+  html = rmarkdown::render(file)
+  ll = readLines(html)
+  ## remove HTML header & footer
+  headerfooter = grep("<!--", ll)
+  ll = ll[(headerfooter[1] + 1) : (headerfooter[2] - 1)]
+  ## remove comments
+  ll = ll[-grep("-->", ll)]
+  ## remove div
+  ll = ll[-grep("<[/]?div", ll)]
+  ## replace headers...
+  ll = sub("<h4>((\\w+ ?)*).*</h4>$", replacement = "<p><strong>\\1</strong></p>", ll)
+  ## ... and package hyperlinks
+  ll = sub("<a href=\"https://cran.+>(\\w+)</a>", replacement = "<pkg>\\1</pkg>", ll)
+
+  # add XML header & footer
+  pkglist = c("<packagelist>", paste0("<pkg>", pkgs, "</pkg>"), "</packagelist>")
+  ll = c("<CRANTaskView>", ll, "</info>", pkglist,
+    "<links><a href=\"http://www.psych.mcgill.ca/misc/fda/\">FDA</a></links>",
+    "</CRANTaskView>")
+
+  if (save) writeLines(ll, "FDA.xml")
+  invisible(ll)
+}
+
+
+
---
+++ update.R
@@ -0,0 +1,18 @@
+source("helper.R")
+
+# get contacts for feedback
+pkgs = extract_pkgs("CRAN_task_view_fda.md")
+not_there <- !(pkgs %in% rownames(available.packages()))
+if (any(not_there)) {
+  cat("###########################")
+  cat(pkgs[not_there])
+  cat("###########################")
+}
+
+install.packages(pkgs[!(pkgs %in% rownames(installed.packages()))])
+maintainers = get_maintainer_mail(pkgs)
+
+# render XML from md
+md2ctv("CRAN_task_view_fda.md")
+# check compliance & render HTML
+ctv::ctv2html("FDA.xml")
